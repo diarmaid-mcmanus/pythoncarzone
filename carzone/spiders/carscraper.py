77c23c69629d9fa13ec2fce9ec39f9dd32867697
Diarmaid McManus <dmcmanus@riotgames.com>
2017-04-09 20:02:44 +0100

    Adding 2nd page scraping, fault handing

--- carzone/spiders/carscraper.py
+++ carzone/spiders/carscraper.py
@@ -10,13 +10,31 @@ class CarscraperSpider(scrapy.Spider):
     current_page = 1
 
     def parse(self, response):
+        def try_except(success, failure, thing):
+            try:
+                return success[thing]
+            except KeyError:
+                return failure
+
         all_data = json.loads(response.text)
         vehicles = all_data['results']
         for vehicle in vehicles:
-            yield {
-                'id': vehicle['id'],
-                'url': vehicle['url'],
-            }
+            try:
+                yield {
+                    'id': vehicle['id'],
+                    'url': vehicle['url'],
+                    'make': vehicle['make'],
+                    'model': vehicle['model'],
+                    'features': vehicle['features'],
+                    'mileage': try_except(vehicle, None, 'km'),
+                    'engine': try_except(vehicle, None, 'engine'),
+                    'price': vehicle['price'],
+                    'year': try_except(vehicle, None, 'year')
+                }
+                yield scrapy.Request(vehicle['url'], callback=self.car_details_parse)
+            except:
+                from scrapy.shell import inspect_response
+                inspect_response(response, self)
 
         if self.current_page < all_data['state']['totalPageNumber']:
             # increment 1 to the current page, format the URL, yield to scrapy
@@ -24,3 +42,18 @@ class CarscraperSpider(scrapy.Spider):
             yield scrapy.Request(next_page, callback=self.parse)
 
         self.current_page += 1
+
+    def car_details_parse(self, response):
+        vehicle_data_html = response.css('script#data::text').extract_first()
+        vehicle_data_trimmed = vehicle_data_html[16:]
+        all_data = json.loads(vehicle_data_trimmed)
+        try:
+            yield {
+                'id': all_data['result']['id'],
+                'image_urls': all_data['result']['largeImageUrls']
+            }
+        except: # TODO no images.
+            yield {
+                'id': all_data['result']['id'],
+                'image_urls': []
+            }
